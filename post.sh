7e415f39576bca06adaaf95c857b65dae1d4a537
f <f@frickelkasten.monolith>
2011-07-05 20:31:32 +0200

    misc

--- post.sh
+++ post.sh
@@ -2,7 +2,6 @@ file=$1
 
 if [ -f "$file" ]; then
 
-#beef guess.bf
-cat $file |cut  -d";" -f6  | awk '{ print "curl -X POST --data \"match_id=123&result=2:1&token=tok\" http://botliga.de/api/guess" }' #|sh
+cat $file |cut  -d";" -f6 |beef pool.bf  #|sh
 
 fi
