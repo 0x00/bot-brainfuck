a4914d70f9dc461a161806ca26bf91ce465c193d
Felix <f@.(none)>
2011-07-14 20:23:14 +0200

    changed file extension

--- post.sh
+++ post.sh
@@ -3,7 +3,7 @@ file=$1
 if [ -f "$file" ]; then
 
 while read line; do
-  echo $line |cut  -d";" -f6 |beef pool.bf | sed "s#=tok#=${token}#g"  |sh
+  echo $line |cut  -d";" -f6 |beef pool.b | sed "s#=tok#=${token}#g"  |sh
 done<$file
 
 
